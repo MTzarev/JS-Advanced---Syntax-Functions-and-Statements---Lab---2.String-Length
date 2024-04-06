e242d9f48fb12df3a4ad5d90906a2a863a7f17c6
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-06 22:15:20 +0300

    Update 2.String Length.js

--- 2.String Length.js
+++ 2.String Length.js
@@ -1,8 +1,6 @@
 function stringLength(str1, str2, str3) {
-    let total = 0;
-    total += str1.length;
-    total += str2.length;
-    total += str3.length;
+    let total = str1.length+str2.length+str3.length;
+    
     let avgSum = Math.floor(total / 3);
 
     console.log(`${total}\n${avgSum}`);
